f64f55b894bfab64e33f2c3e36f32be1e8c32482
gateTang <gate.tang@gmail.com>
2022-06-25 22:01:18 +0700

    json key removed

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from firebase_admin import firestore
 
 from flask import Flask, request, jsonify, render_template
 from flask_mqtt import Mqtt
+import numpy as np
 import json
 from datetime import datetime
 import io
@@ -15,18 +16,25 @@ import matplotlib.pyplot as plt
 
 matplotlib.use('Agg')
 
+#JSON key for firebase.
 cred = credentials.Certificate('fish-key.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
+#Initialise firebase documents.
 doc_ref = db.collection(u'users').document(u'tempData')
 date_ref = db.collection(u'users').document (u'date')
 
 date = datetime.now().day
 date_ref.set({u'date': date})
 
+thislist = []
+
+#Flask framework.
 app = Flask(__name__)
+
+#MQTT setup.
 app.config['MQTT_BROKER_URL'] = 'maqiatto.com'
 app.config['MQTT_BROKER_PORT'] = 1883
 app.config['MQTT_USERNAME'] = 'gate.tang@gmail.com'
@@ -34,6 +42,7 @@ app.config['MQTT_PASSWORD'] = 'letmein'
 app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
 mqtt = Mqtt(app)
 
+#Flask framework routes. 
 @app.route('/readdata', methods=['GET']) #path of link. 
 def respond():
     doc = doc_ref.get()
@@ -49,19 +58,19 @@ def hub():
 def handle_connect(client, userdata, flags, rc):
     mqtt.subscribe('gate.tang@gmail.com/LED')
 
+@mqtt.on_message()
 def time():
     #updates time stamps every hour E.g. 1AM, 2AM everytime a MQTT Message comes in. 
-    time = datetime.now().strftime("%I%p")
-    thislist = []
+    time = datetime.now().strftime("%H%M")
     thislist.append(time)
     return thislist
 
-@mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
     x = datetime.now().day
     dateD = date_ref.get()
     dateC = dateD.to_dict()
     previous = dateC["date"]
+    #Erase data after one day. 
     if (previous != x):
         doc_ref.set({})
         thislist=[]
@@ -87,7 +96,8 @@ def graph():
 
     for key in data:
         dataarray[int(key)] = data[key] #Orders converted array into order
-
+    
+    #Creates graph.
     plt.clf()
     z = datetime.now()
     date = z.strftime("%B"+"%d"+"-%y")
@@ -96,21 +106,23 @@ def graph():
     plt.title("Aquarium Temp Over Time - "+date)
     plt.grid(True)
     plt.savefig(date+'waterTemp.png')
-    plt.plot(dataarray)
+    plt.plot(dataarray, 'bx')
     fig = plt.gcf()
 
     amount = len(dataarray)-1
     current_amount = (dataarray[amount])
-
-    thislist = time()
+    if dataarray == []:
+        current_amount - 0
+    else:
+        current_amount = (dataarray[amount])
 
     buf = io.BytesIO()
     fig.savefig(buf, format='png')
     buf.seek(0)
     string = base64.b64encode(buf.read())
     url = urllib.parse.quote(string)
-    
-    Dict = { i : dataarray[i] for i in range(0, len(dataarray) ) }
+
+    Dict = { i : dataarray[i] for i in range(0,len(dataarray) ) }
     xy=Dict.items()
     reset_response = "RESET Attempted"
     if request.method == 'POST':
